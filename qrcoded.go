ed618b2ee53b63b59ad75511a8f346c2aca8a2e6
Trenell Galman <trenellgalman@gmail.com>
2015-09-17 22:40:31 -0400

    Changed GenerateQRCode function for error handling changes. Tests now pass.

--- qrcoded.go
+++ qrcoded.go
@@ -19,6 +19,5 @@ func main() {
 
 func GenerateQRCode(w io.Writer, code string) error {
 	img := image.NewNRGBA(image.Rect(0, 0, 21, 21))
-	_ = png.Encode(w, img)
-	return nil
+	return png.Encode(w, img)
 }
